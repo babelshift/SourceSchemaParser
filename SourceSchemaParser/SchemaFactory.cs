c9b7d006bca9f910dee44e9529a5950322c66224
babelshift <justin.d.skiles@gmail.com>
2015-11-27 21:10:48 -0500

    Add regions to understand code. Add public factory methods to create leagues from VDF text or VDF file.

--- SourceSchemaParser/SchemaFactory.cs
+++ SourceSchemaParser/SchemaFactory.cs
@@ -11,42 +11,47 @@ namespace SourceSchemaParser
 {
     public static class SchemaFactory
     {
-        public static IReadOnlyCollection<DotaLeague> GetDotaLeagues(string schemaFilePath, string languageFilePath)
+        #region Public Entry Points
+
+        public static IReadOnlyCollection<DotaLeague> GetDotaLeaguesFromText(string vdfText, string languageFilePath)
+        {
+            var leaguesFromSchema = GetLeaguesFromItemSchema(vdfText);
+
+            return GetDotaLeagues(languageFilePath, leaguesFromSchema);
+        }
+
+        public static IReadOnlyCollection<DotaLeague> GetDotaLeaguesFromFile(string schemaFilePath, string languageFilePath)
         {
             var vdfTextLines = File.ReadAllLines(schemaFilePath);
+            var leagesFromSchema = GetLeaguesFromItemSchema(vdfTextLines);
 
-            var leagues = GetLeaguesFromSchema(vdfTextLines);
+            return GetDotaLeagues(languageFilePath, leagesFromSchema);
+        }
 
+        #endregion
+
+        #region Merge Parsed Leagues and Language Tokens
+
+        private static IReadOnlyCollection<DotaLeague> GetDotaLeagues(string languageFilePath, IReadOnlyCollection<DotaSchemaItem> parsedDotaLeagues)
+        {
             var tokens = GetLanguageTokensFromLanguageSchema(languageFilePath);
 
-            ReplaceTokensWithLocalizedValues(leagues, tokens);
+            ReplaceTokensWithLocalizedValues(parsedDotaLeagues, tokens);
 
-            List<DotaLeague> dotaLeagues = FlattenDotaSchemaItemLeagues(leagues);
-
-            return dotaLeagues;
-        }
-
-        private static List<DotaLeague> FlattenDotaSchemaItemLeagues(IReadOnlyCollection<DotaSchemaItem> leagues)
-        {
-            List<DotaLeague> dotaLeagues = new List<DotaLeague>();
-            foreach (var league in leagues)
-            {
-                DotaLeague dotaLeague = new DotaLeague(league);
-                dotaLeagues.Add(dotaLeague);
-            }
+            List<DotaLeague> dotaLeagues = FlattenDotaSchemaItemLeagues(parsedDotaLeagues);
 
             return dotaLeagues;
         }
-
+        
         private static void ReplaceTokensWithLocalizedValues(IReadOnlyCollection<DotaSchemaItem> leagues, IDictionary<string, string> tokens)
         {
             foreach (var league in leagues)
             {
-                league.NameLocalized = GetToken(league.NameLocalized.Remove(0, 1), tokens);
-                league.DescriptionLocalized = GetToken(league.DescriptionLocalized.Remove(0, 1), tokens);
+                league.NameLocalized = GetLanguageToken(league.NameLocalized.Remove(0, 1), tokens);
+                league.DescriptionLocalized = GetLanguageToken(league.DescriptionLocalized.Remove(0, 1), tokens);
                 if (!String.IsNullOrEmpty(league.TypeName))
                 {
-                    league.TypeName = GetToken(league.TypeName.Remove(0, 1), tokens);
+                    league.TypeName = GetLanguageToken(league.TypeName.Remove(0, 1), tokens);
                 }
                 else
                 {
@@ -55,7 +60,7 @@ namespace SourceSchemaParser
             }
         }
 
-        private static string GetToken(string key, IDictionary<string, string> tokens)
+        private static string GetLanguageToken(string key, IDictionary<string, string> tokens)
         {
             string localizedValue = String.Empty;
             bool exists = tokens.TryGetValue(key, out localizedValue);
@@ -80,10 +85,27 @@ namespace SourceSchemaParser
             return tokens;
         }
 
-        private static IReadOnlyCollection<DotaSchemaItem> GetLeaguesFromSchema(string[] vdfTextLines)
+
+        #endregion
+
+        #region Parse Out Leagues from VDF and JSON
+
+        private static IReadOnlyCollection<DotaSchemaItem> GetLeaguesFromItemSchema(string vdfText)
+        {
+            string json = VDFConverter.ToJson(vdfText);
+
+            return GetLeaguesFromJson(json);
+        }
+
+        private static IReadOnlyCollection<DotaSchemaItem> GetLeaguesFromItemSchema(string[] vdfTextLines)
         {
             string json = VDFConverter.ToJson(vdfTextLines);
 
+            return GetLeaguesFromJson(json);
+        }
+
+        private static IReadOnlyCollection<DotaSchemaItem> GetLeaguesFromJson(string json)
+        {
             JObject itemSchema = JObject.Parse(json);
 
             JToken item = itemSchema["items_game"]["items"];
@@ -91,6 +113,20 @@ namespace SourceSchemaParser
             var leagues = JsonConvert.DeserializeObject<IList<DotaSchemaItem>>(item.ToString(), new SchemaItemsToDotaLeaguesJsonConverter());
 
             return new ReadOnlyCollection<DotaSchemaItem>(leagues);
+        }
+
+        #endregion
+        
+        private static List<DotaLeague> FlattenDotaSchemaItemLeagues(IReadOnlyCollection<DotaSchemaItem> leagues)
+        {
+            List<DotaLeague> dotaLeagues = new List<DotaLeague>();
+            foreach (var league in leagues)
+            {
+                DotaLeague dotaLeague = new DotaLeague(league);
+                dotaLeagues.Add(dotaLeague);
+            }
+
+            return dotaLeagues;
         }
     }
 }
