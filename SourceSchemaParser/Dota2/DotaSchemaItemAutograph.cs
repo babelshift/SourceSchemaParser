581fa8ca189b8b947c373b765b1247537990d9f1
babelshift <justin.d.skiles@gmail.com>
2015-12-18 12:39:37 -0500

    Nullable workshop link on autographs. Added name modifier serialization on autographs.

--- SourceSchemaParser/Dota2/DotaSchemaItemAutograph.cs
+++ SourceSchemaParser/Dota2/DotaSchemaItemAutograph.cs
@@ -14,10 +14,12 @@ namespace SourceSchemaParser.Dota2
         [JsonProperty("autograph")]
         public string Autograph { get; set; }
         [JsonProperty("workshoplink")]
-        public long WorkshopLink { get; set; }
+        public long? WorkshopLink { get; set; }
         [JsonProperty("language")]
         public int Language { get; set; }
         [JsonProperty("icon_path")]
         public string IconPath { get; set; }
+        [JsonProperty("name_modifier")]
+        public string Modifier { get; set; }
     }
 }
